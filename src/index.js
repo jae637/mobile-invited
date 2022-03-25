ed6159950d2b504ccf6ff3b98b4ff7b546c3127a
Karl <jae637@naver.com>
2022-03-25 22:54:37 +0900

    ie 대응 완료

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
