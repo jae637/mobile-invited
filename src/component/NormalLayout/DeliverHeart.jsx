9947fd6aa38ea0c47126a11555ee74a4fe3189bf
Karl <jae637@naver.com>
2022-12-04 22:15:33 +0900

    [UPDATE] 마음 전하는 곳 업데이트

--- src/component/NormalLayout/DeliverHeart.jsx
+++ src/component/NormalLayout/DeliverHeart.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { Card, ListGroup } from 'react-bootstrap';
 
 import titleImg from 'asset/images/00title.png';
-import { FaRegCopy } from 'react-icons/fa';
 import MovingDot from './Main/MovingDot';
+import { FaRegCopy } from 'react-icons/fa';
+import { useParams, useNavigate } from 'react-router-dom';
 
 function DeliverHeart(props) {
+    const navigation = useNavigate()
+    const navigate = useParams();
+    const personalName = navigate.name
+
     function clipboard(text) {
         var aux = document.createElement("textarea");
         aux.value = text;
@@ -17,32 +22,33 @@ function DeliverHeart(props) {
         alert("복사가 완료되었습니다.")
     }
     return (
-        <div className="d-flex flex-column align-content-center py-5"
-            style={{ textAlign: 'center', width: window.innerWidth, height: window.innerHeight, backgroundColor: '#e3b2ad' }}>
+        <div className="d-flex flex-column align-content-center py-5">
 
             <div className="background-banner d-flex d-row align-items-top justify-content-center">
-                <img src={titleImg} alt="title" />
+                <img src={titleImg} alt="title" onClick={() => { navigation("../") }} />
             </div>
 
             <div className="d-flex align-items-top justify-content-center">
                 <MovingDot />
                 <div className='speech-balloon py-2 d-flex align-items-baseline mt-4' >
                     <div className="p-2 bg-white" style={{ borderRadius: 8, fontSize: '0.8rem' }}>
-                        감사합니다.
+                        '{personalName}' 님의 <br />
+                        따뜻한 마음 <br />
+                        고맙습니다.
                     </div>
                 </div>
             </div>
             <div className="align-self-center">
                 <Card style={{ width: '100%' }} className={"my-2 "}>
                     <Card.Body>
-                        <Card.Title>신랑 측 마음 전하는 곳</Card.Title>
+                        <Card.Title className="text-center">신랑 측 마음 전하는 곳</Card.Title>
                         <ListGroup className="list-group-flush">
                             <ListGroup.Item className="d-flex justify-content-between align-items-center" onClick={() => { clipboard("우리은행 1002961499443") }}>
                                 <span>
                                     신랑 최재형 <br />
                                     우리은행 : 1002-961-499443
                                     </span>
-                                <span>
+                                <span className="ps-4">
                                     <FaRegCopy />
                                 </span>
                             </ListGroup.Item>
@@ -51,14 +57,14 @@ function DeliverHeart(props) {
                 </Card>
                 <Card style={{ width: '100%' }} className={"my-2"}>
                     <Card.Body>
-                        <Card.Title>신부 측 마음 전하는 곳</Card.Title>
+                        <Card.Title className="text-center">신부 측 마음 전하는 곳</Card.Title>
                         <ListGroup className="list-group-flush">
                             <ListGroup.Item className="d-flex justify-content-between align-items-center" onClick={() => { clipboard("카카오뱅크 3333030755400") }}>
                                 <span>
                                     신부 한소람 <br />
                                     카카오뱅크 : 3333-030-755400
                                 </span>
-                                <span>
+                                <span className="ps-4">
                                     <FaRegCopy />
                                 </span>
                             </ListGroup.Item>
